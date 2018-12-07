771654e8423b728b17617d9c32b5493e209442f8
Nilesh <nileshborse65@gmail.com>
2018-12-07 16:14:04 +0530

    Latest changes

--- core/console/cache_console.php
+++ core/console/cache_console.php
@@ -2,7 +2,7 @@
 /**
  * KumbiaPHP web & app Framework
  *
- * LICENSE
+ * LICENSE 
  *
  * This source file is subject to the new BSD license that is bundled
  * with this package in the file LICENSE.txt.
