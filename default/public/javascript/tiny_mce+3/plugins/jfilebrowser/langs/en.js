3fa4e7291ea6f95cbe3f9adc7e80147efc52ee83
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-01 23:37:56 +0800

    Delete default directory

--- default/public/javascript/tiny_mce+3/plugins/jfilebrowser/langs/en.js
+++
@@ -1,4 +0,0 @@
-tinyMCE.addI18n('en.jfilebrowser',{
-	desc : 'This is just a template button',
-	deleteImg : 'Estas seguro'
-});
--- default/public/javascript/tiny_mce+3/plugins/jfilebrowser/langs/en_dlg.js
+++
@@ -1,3 +0,0 @@
-tinyMCE.addI18n('en.jfilebrowser_dlg',{
-	title : 'jFileBrowser'
-});
